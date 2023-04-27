32d91ec7506e9e1e5b2783b536ade93bb5bc3c89
moulee96 <63225402+moulee96@users.noreply.github.com>
2023-04-27 09:43:44 +0530

    Active links

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a').forEach(link => {
-    if(link.href.includes(`${activePage}`)){
-        link.classList.add('active');
-    }
-})
+
+const navLinks = document.querySelectorAll("nav a").forEach((link) => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add("active");
+  }
+});
